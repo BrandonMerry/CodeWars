ddf74be1d9414e3c375cef15270c9338569a5f40
Brandon Merry <Brandon.l.merry@gmail.com>
2022-04-01 11:02:11 +0100

    2nd & 3rd done

--- 1-Apr-22.js
+++ 1-Apr-22.js
@@ -3,3 +3,20 @@
 function numberToString(num) {
     return num.toString();
   }
+
+  const numberToString = num => `${num}`;
+
+//   Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+function boolToWord( bool ){
+    if (bool == true){
+    return "Yes"} 
+    else{
+  return "No"}
+  }
+
+//   Complete the solution so that it reverses the string passed into it. eg. "car" ->"rac"
+
+function solution(str){
+    return str.split('').reverse().join('')
+  }
