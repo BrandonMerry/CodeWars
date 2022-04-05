5881bef65ac08fb2c01e2ee64434573b1e856f0f
Brandon Merry <Brandon.l.merry@gmail.com>
2022-04-05 10:26:41 +0100

    repeate a string function

--- 5-Apr-22.js
+++ 5-Apr-22.js
@@ -1,9 +1,18 @@
 // remove spaces in the string
 
-// function noSpace(x){
-//     return x = x.replace(/\s+/g, '');
-//    }
+function noSpace(x){
+    return x = x.replace(/\s+/g, '');
+   }
 
 //    arrow function 
 
  noSpace = x => x.replace(/\s+/g, '');
+
+
+//  repeat a string 
+
+function repeatStr (n, s) {
+    return s.repeat(n);
+  }
+
+  repeatStr = (n, s) => s.repeat(n)
