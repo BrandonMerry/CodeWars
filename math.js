c9edf73a665e8910739e60714e2573a5ead7de2b
Brandon Merry <Brandon.l.merry@gmail.com>
2022-04-14 11:29:13 +0100

    math.celi

--- math.js
+++ math.js
@@ -5,3 +5,12 @@
 function litres(time) {
     return Math.floor(time * 0.5);
   }
+
+  //Given a year, return the century it is in.
+
+  function century(year) {
+    // Finish this :)
+    return Math.ceil(year/100)
+  }
+
+  const century = year => Math.ceil(year/100)
