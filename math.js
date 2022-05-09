e77a299b3ba666efd91bb69b80ffef0e50da973b
Brandon Merry <Brandon.l.merry@gmail.com>
2022-05-09 21:17:53 +0700

    sum positive array

--- math.js
+++ math.js
@@ -14,3 +14,20 @@ function litres(time) {
   }
 
   const century = year => Math.ceil(year/100)
+
+
+  // sum the positive components of an array 
+
+  function positiveSum(arr) {
+    var total = 0;    
+    for (i = 0; i < arr.length; i++) {    // setup loop to go through array of given length
+      if (arr[i] > 0) {                   // if arr[i] is greater than zero
+        total += arr[i];                  // add arr[i] to total
+      }
+    }
+    return total;                         // return total
+  }
+
+  function positiveSum(arr) {
+    return arr.filter(x => x > 0).reduce((a, b) => a+b, 0);
+  }
