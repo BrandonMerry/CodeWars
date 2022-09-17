f81ef8232119f77bbd6d04529c1a82e618c43873
brandon <brandon.l.merry@gmail.com>
2022-09-17 23:02:54 +0700

    using the join method

---
+++ Arrays/join.js
@@ -0,0 +1,12 @@
+// Printing Array elements with Comma delimiters
+
+// Input: Array of elements
+
+// ["h","o","l","a"]
+
+// Output: String with comma delimited elements of the array in th same order.
+
+// "h,o,l,a"
+
+
+printArray = array => array.join()
