169339ff6e2846142f478d519c5fd91c472d1721
brandon <brandon.l.merry@gmail.com>
2022-10-17 18:08:06 +0700

    did PREP for this problem

---
+++ Arrays/7kyu/sort_arrays.js
@@ -0,0 +1,16 @@
+// Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+
+// Params
+
+// does the array include letters, numbers and special characters
+//is the data only in strings?
+// will the data have invalid input
+
+// returns
+//what do you want returned ?
+
+//examples
+//[2,6,7,1,9] => [1,2,6,7,9]
+//[12,43,56,1,7,90,15,30] => [1,7,12,15,30,43,56,90]
+
+//
